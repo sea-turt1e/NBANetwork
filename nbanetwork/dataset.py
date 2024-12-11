278838395d5c2dc8934f0628f6e87cfcfea38ddc
sea-turt1e <h.yamada.bg@gmail.com>
2024-12-12 07:05:08 +0900

    Add command to process player nodes dataset and handle undrafted players

--- nbanetwork/dataset.py
+++ nbanetwork/dataset.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import typer
+import ipdb
 from loguru import logger
 from tqdm import tqdm
 
@@ -116,6 +117,57 @@ def player_network_dataset(
     edges_df.to_csv(edge_output_path, index=False)
 
     print("ノード属性データとエッジデータの生成が完了しました。")
+
+
+@app.command(name="process_player_nodes_dataset")
+def process_player_nodes_dataset(
+    input_path: Path = INTERIM_DATA_DIR / "player_nodes.csv",
+    output_path: Path = PROCESSED_DATA_DIR / "player_nodes.csv",
+):
+    import random
+    import pandas as pd
+    import torch
+
+    # read nodes data
+    node_df = pd.read_csv(input_path)
+
+    # process undrafted players
+    node_df["is_undrafted"] = node_df["draft_year"].isna().astype(int)
+    # If draft_year is Undrafted, replace with NaN
+    node_df["draft_year"] = node_df["draft_year"].replace("Undrafted", pd.NA)
+    # If draft_year is NaN, use the first season. For example, 1996-1997 becomes 1996
+    node_df["draft_year"] = node_df["draft_year"].fillna(node_df["season"].str.split("-").str[0])
+    # However, since the season is multiple years for the same player, group by player_name and take the first value
+    node_df["draft_year"] = node_df.groupby("player_name")["draft_year"].transform("first")
+    # Assume that players who are not drafted are in the 3rd round
+    node_df["draft_round"] = node_df["draft_round"].replace("Undrafted", 3)
+    # Assign draft_number randomly between 61 and 90
+    node_df["draft_number"] = node_df["draft_number"].replace("Undrafted", random.randint(61, 90))
+
+    # Convert columns that can be converted to numbers
+    numeric_columns = [
+        "age",
+        "player_height",
+        "player_weight",
+        "draft_year",
+        "draft_round",
+        "draft_number",
+        "gp",
+        "pts",
+        "reb",
+        "ast",
+        "net_rating",
+        "oreb_pct",
+        "dreb_pct",
+        "usg_pct",
+        "ts_pct",
+        "ast_pct",
+    ]
+    for col in numeric_columns:
+        node_df[col] = pd.to_numeric(node_df[col], errors="coerce")
+    node_df.to_csv(output_path, index=False)
+
+    logger.success("Node data processing complete.")
 
 
 if __name__ == "__main__":
