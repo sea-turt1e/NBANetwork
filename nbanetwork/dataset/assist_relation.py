5104a5132bc11dcb3f75a30357324c66d36bd1c1
sea-turt1e <h.yamada.bg@gmail.com>
2024-12-26 19:45:47 +0900

    update: refactor assist relation extraction to improve variable naming and logging

--- nbanetwork/dataset/assist_relation.py
+++ nbanetwork/dataset/assist_relation.py
@@ -7,19 +7,20 @@ import typer
 from loguru import logger
 from tqdm import tqdm
 
-from nbanetwork.config import INTERIM_DATA_DIR, PROCESSED_DATA_DIR, RAW_DATA_DIR
+from nbanetwork.config import INTERIM_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
 
 @app.command()
 def main(
-    input_path: str = RAW_DATA_DIR / "nbadatabase" / "csv" / "play_by_play.csv",
-    output_path: str = INTERIM_DATA_DIR / "players" / "assist_relation.csv",
+    input_path: Path = RAW_DATA_DIR / "nbadatabase" / "csv" / "play_by_play.csv",
+    output_path: Path = INTERIM_DATA_DIR / "players" / "assist_relation.csv",
     is_debug: bool = False,
 ):
 
     # read csv
+    logger.info("Reading data")
     if is_debug:
         df = pd.read_csv(input_path, nrows=10000)
     else:
@@ -52,9 +53,9 @@ def main(
             return pd.Series(
                 {
                     "game_id": row["game_id"],  # game id
-                    "player": row["player1_name"],  # player who made the shot
-                    "player_team_abbreviation": row["player1_team_abbreviation"],  # team of the player
-                    "player1_position": row["person1type"],
+                    "scorer": row["player1_name"],  # player who made the shot
+                    "scorer_team_abbreviation": row["player1_team_abbreviation"],  # team of the player
+                    "scorer_position": row["person1type"],
                     "assister": row["player2_name"],  # player who made the assist
                     "assister_team_abbreviation": row["player2_team_abbreviation"],  # team of the assister
                     "assister_position": row["person2type"],
@@ -66,10 +67,10 @@ def main(
     # apply the function to each row
     tqdm.pandas(desc="Extracting assist info")
     assist_info = assist_data.progress_apply(extract_assist_info, axis=1)
-    print(f"{num=}")
+    logger.info(f"Number of assist: {num}")
 
     with open(output_path, "w") as f:
         f.write(
-            "game_id,player,player_team_abbreviation,player1_position,assister,assister_team_abbreviation,assister_position,shot_type,season\n"
+            "game_id,scorer,scorer_team_abbreviation,scorer_position,assister,assister_team_abbreviation,assister_position,shot_type,season\n"
         )
         assist_info.to_csv(f, header=False, index=False)
