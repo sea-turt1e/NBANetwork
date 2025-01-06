bb37735c975edab5c66cb9d7d1f95106c84be86a
sea-turt1e <h.yamada.bg@gmail.com>
2025-01-06 20:48:42 +0900

    update: add edge ratio parameter and refactor edge processing scripts for player assists

--- scripts/player_chemistry/dataset/increase_neg_other_team.sh
+++ scripts/player_chemistry/dataset/increase_neg_other_team.sh
@@ -5,6 +5,7 @@ year_from=1996
 year_until=2022
 year_last=2023
 is_debug=""
+edge_ratio=1.5
 
 # parse command line arguments
 while [ "$#" -gt 0 ]; do
@@ -25,6 +26,10 @@ while [ "$#" -gt 0 ]; do
       is_debug="--is-debug"
       shift 1
       ;;
+    --edge-ratio)
+      edge_ratio="$2"
+      shift 2
+      ;;
     *)
       echo "Unknown argument: $1"
       exit 1
@@ -33,15 +38,19 @@ while [ "$#" -gt 0 ]; do
 done
 
 # for train
-typer nbanetwork/dataset/increase_neg_edge.py run \
+typer nbanetwork/dataset/increase_edge_by_assist.py run \
 data/processed/players/player_nodes_${year_from}-${year_until}.csv \
-data/interim/players/player_edges_pos_${year_from}-${year_until}.csv \
-data/interim/players/player_edges_neg_${year_from}-${year_until}.csv \
-data/processed/players
+data/interim/players/same_team_pos_assist_edges_same_team_${year_from}-${year_until}.csv \
+data/interim/players/same_team_neg_assist_edges_same_team_${year_from}-${year_until}.csv \
+data/processed/players/assist_edges_pos_${year_from}-${year_until}.csv \
+data/processed/players/assist_edges_neg_${year_from}-${year_until}.csv \
+--edge-ratio $edge_ratio
 
 # for test
-typer nbanetwork/dataset/increase_neg_edge.py run \
+typer nbanetwork/dataset/increase_edge_by_assist.py run \
 data/processed/players/player_nodes_${year_until}-${year_last}.csv \
-data/interim/players/player_edges_pos_${year_until}-${year_last}.csv \
-data/interim/players/player_edges_neg_${year_until}-${year_last}.csv \
-data/processed/players  
+data/interim/players/same_team_pos_assist_edges_same_team_${year_until}-${year_last}.csv \
+data/interim/players/same_team_neg_assist_edges_same_team_${year_until}-${year_last}.csv \
+data/processed/players/assist_edges_pos_${year_until}-${year_last}.csv \
+data/processed/players/assist_edges_neg_${year_until}-${year_last}.csv \
+--edge-ratio $edge_ratio
