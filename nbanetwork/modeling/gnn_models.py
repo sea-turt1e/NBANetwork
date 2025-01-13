0caa621c8686c652391be228cd8d8cd8311cad68
sea-turt1e <h.yamada.bg@gmail.com>
2025-01-13 13:32:55 +0900

    update: implement GraphSAGE model and replace GCN usage in training scripts

--- nbanetwork/modeling/gnn_models.py
+++ nbanetwork/modeling/gnn_models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GATConv, GCNConv
+from torch_geometric.nn import GATConv, GCNConv, SAGEConv
 
 
 class GCN(torch.nn.Module):
@@ -40,21 +40,41 @@ class GCN(torch.nn.Module):
         return scores
 
 
-# class GAT(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, dropout=0.5):
-#         super(GAT, self).__init__()
-#         self.conv1 = GATConv(in_channels, hidden_channels, heads=4, dropout=dropout)
-#         self.bn1 = torch.nn.BatchNorm1d(hidden_channels * 4)
-#         self.conv2 = GATConv(hidden_channels * 4, hidden_channels * 2, heads=4, dropout=dropout)
-#         self.bn2 = torch.nn.BatchNorm1d(hidden_channels * 2)
-#         self.conv3 = GATConv(hidden_channels * 2, hidden_channels * 2, heads=4, dropout=dropout)
-#         self.bn3 = torch.nn.BatchNorm1d(hidden_channels * 2)
-#         self.dropout = dropout
-#         self.fc = torch.nn.Sequential(
-#             torch.nn.Linear(hidden_channels * 4, hidden_channels * 2),
-#             torch.nn.ReLU(),
-#             torch.nn.Linear(hidden_channels * 2, 1),
-#         )
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, dropout=0.5):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(in_channels, hidden_channels)
+        self.bn1 = torch.nn.BatchNorm1d(hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels * 2)
+        self.bn2 = torch.nn.BatchNorm1d(hidden_channels * 2)
+        self.conv3 = SAGEConv(hidden_channels * 2, hidden_channels * 2)
+        self.bn3 = torch.nn.BatchNorm1d(hidden_channels * 2)
+        self.dropout = dropout
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(hidden_channels * 4, hidden_channels * 2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_channels * 2, 1),
+        )
+
+    def forward(self, x, edge_index, edge_weight=None):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.bn1(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.bn2(x)
+        x = self.conv3(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.bn3(x)
+        return x
+
+    def score(self, z, src, dst):
+        edge_emb = torch.cat([z[src], z[dst]], dim=1)
+        scores = self.fc(edge_emb).squeeze()
+        return scores
 
 
 class GAT(torch.nn.Module):
